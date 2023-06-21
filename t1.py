9dd65a57c2f8a392b9c5c5e2adda8125c219deaa
nistha <nishthakashyap2@gmail.com>
2023-06-21 15:37:21 +0530

    change

--- t1.py
+++ t1.py
@@ -11,19 +11,7 @@ lt(90)
 mainloop()# this line is needed to keep the window open
 
 
-angle= 360/6
-fd(100)
-rt(angle)
-fd(100)
-rt(angle)
-fd(100)
-rt(angle)
-fd(100)
-rt(angle)
-fd(100)
-rt(angle)
-fd(100)
-rt(angle)
+
 
 
 
